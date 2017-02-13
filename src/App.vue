c1c565d027bb3b5652b6e29932f8ed601a699cab
Grant Freeman <grant.c.freeman@raytheon.com>
2017-02-13 14:31:13 -0600

    implemented rollupjs for Vue SFC

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>{{ greeting }}</h1>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            greeting: 'Hello World!'
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    text-align: center;
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  data: {
+    name: 'world'
+  },
+  render: h => h(App)
+})
